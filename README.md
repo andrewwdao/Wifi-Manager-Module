382c691d94b023aa4ce0dbecc1ad817bde7bf31a
Minh An Dao <minhan7497@gmail.com>
2019-09-10 23:12:12 +0700

    first commit adapted from Tony Pottier - Thank you!

---
+++ README.md
@@ -0,0 +1,7 @@
+HEAT CONTROLLER SYSTEM
+
+This is an embedded code of the Heat Controller System, designed for Pomelo Co.
+
+Last update: 08/2019
+
+(c) Can Tho University - 2019
---
+++ workspace/build/esp32-wifi-manager-master/component_project_vars.mk
@@ -0,0 +1,7 @@
+# Automatically generated build file. Do not edit.
+COMPONENT_INCLUDES += /c/Users/minha/Desktop/esp32-wifi-manager-master/src
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/esp32-wifi-manager-master -lesp32-wifi-manager-master
+COMPONENT_LINKER_DEPS += 
+COMPONENT_SUBMODULES += 
+COMPONENT_LIBRARIES += esp32-wifi-manager-master
+component-esp32-wifi-manager-master-build: component-log-build
